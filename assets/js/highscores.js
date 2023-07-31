98a9aa398e43d3e512346ce1d73981f7abe141bc
Krystal Richardson <krystal.williams2019@gmail.com>
2023-07-31 19:33:46 -0400

    updated styling for quiz

---
+++ assets/js/highscores.js
@@ -0,0 +1,13 @@
+// high scores will render on the high score page
+renderHighscores();
+
+function renderHighscores() {
+    var highScoresList = JSON.parse(localStorage.getItem("highScoresList"));
+    if (highScoresList !== null) {
+        for (var i = 0; i < highScoresList.length; i++) {
+            var li = document.createElement("li");
+            li.textContent = i+1 + ". " + highScoresList[i].name + " - " + highScoresList[i].score;
+            scoreList.appendChild(li);
+        }
+    }
+}
